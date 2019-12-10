78c9dfc9253811e2ad4bbfff198b1b998d4fa36a
Tajwinder Singh <tajwindersingh@Tajwinders-MacBook-Pro.local>
2019-12-10 18:11:47 -0500

    added form layout and theme

--- src/App.js
+++ src/App.js
@@ -1,13 +1,44 @@
 import React from "react";
+import { createMuiTheme } from "@material-ui/core/styles";
+import { ThemeProvider } from "@material-ui/styles";
+import Container from "@material-ui/core/Container";
+import Grid from "@material-ui/core/Grid";
+
+import CreateArtPage from "./components/pages/CreateArtPage";
+
 import "./App.css";
 
-import CreateArtPage from "./components/CreateArtPage";
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      light: "#2c2c2c",
+      main: "#000000",
+      dark: "#000000",
+      contrastText: "#ffffff"
+    },
+    secondary: {
+      light: "#ff5c7c",
+      main: "#ff0350",
+      dark: "#c40028",
+      contrastText: "#000000"
+    },
+    error: {
+      main: "#18ffff"
+    }
+  }
+});
 
 function App() {
   return (
-    <div>
-      <CreateArtPage />
-    </div>
+    <ThemeProvider theme={theme}>
+      <Container>
+        <Grid container justify={"center"} alignItems={"center"}>
+          <Grid item xs={6}>
+            <CreateArtPage />
+          </Grid>
+        </Grid>
+      </Container>
+    </ThemeProvider>
   );
 }
 
---
+++ src/components/pages/CreateArtPage.js
@@ -0,0 +1,83 @@
+import React from "react";
+import { Formik, Field, Form, ErrorMessage } from "formik";
+import * as Yup from "yup";
+import { TextField } from "formik-material-ui";
+import Typography from "@material-ui/core/Typography";
+import ImageIcon from "@material-ui/icons/Image";
+import Button from "@material-ui/core/Button";
+import Paper from "@material-ui/core/Paper";
+import { makeStyles } from "@material-ui/core/styles";
+
+import "typeface-roboto";
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    padding: theme.spacing(3, 2)
+  }
+}));
+
+const CreateArtForm = () => {
+  const classes = useStyles();
+
+  return (
+    <Formik
+      initialValues={{ name: "", description: "", artworkImage: "" }}
+      validationSchema={Yup.object({
+        name: Yup.string().required(),
+        description: Yup.string()
+          .max(50)
+          .required(),
+        artworkImage: Yup.mixed().required("Artwork Image file required")
+      })}
+      onSubmit={(values, { setSubmitting }) => {
+        setTimeout(() => {
+          alert(JSON.stringify(values, null, 2));
+          setSubmitting(false);
+        }, 400);
+      }}
+    >
+      {({ isSubmitting }) => (
+        <>
+          <Typography variant="h4" gutterBottom>
+            Add Artwork
+          </Typography>
+          <Paper className={classes.root}>
+            <Form>
+              <Field
+                name="name"
+                type="text"
+                component={TextField}
+                label="Name"
+                fullWidth
+              />
+              <Field
+                name="description"
+                type="text"
+                multiline
+                rowsMax="4"
+                component={TextField}
+                label="Description"
+                fullWidth
+              />
+              <ImageIcon />
+              <Field name="artworkImage" type="file" onBlur={null} />
+              <Typography variant="caption" display="block" color="error">
+                <ErrorMessage name="artworkImage" />
+              </Typography>
+              <Button
+                variant="outlined"
+                color="primary"
+                type="submit"
+                disabled={isSubmitting}
+              >
+                submit
+              </Button>
+            </Form>
+          </Paper>
+        </>
+      )}
+    </Formik>
+  );
+};
+
+export default CreateArtForm;
